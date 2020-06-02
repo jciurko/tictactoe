476e5227890307ea8b2adbf53c9b3770d3f6b2ba
ciurkoj <jakub1798@gmail.com>
2020-06-02 15:39:26 +0100

    Updates

---
+++ precache-manifest.71f2a524dae5ddd216de5f2c5aeeddc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5531c08eea83e2fcf0011e8e2672ad86",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "424ae795c327e7b56252",
+    "url": "/tictactoe/static/css/main.29377964.chunk.css"
+  },
+  {
+    "revision": "9db5d9ae66451d9be0e5",
+    "url": "/tictactoe/static/js/2.d285dfba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.d285dfba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "424ae795c327e7b56252",
+    "url": "/tictactoe/static/js/main.bb608239.chunk.js"
+  },
+  {
+    "revision": "1d8dada0581fd2a39a6b",
+    "url": "/tictactoe/static/js/runtime-main.d9939438.js"
+  }
+]);
